257d24e0f9035e515f19234d0697b65d2e135571
pablo-the-souza <53923086+pablo-the-souza@users.noreply.github.com>
2022-06-06 20:46:32 +1000

    Controller

---
+++ Coffee.Api/Coffee.Api/Controllers/CoffeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CoffeeController : ControllerBase
+{
+   
+    public CoffeeController()
+    {
+        
+    }
+}
+
