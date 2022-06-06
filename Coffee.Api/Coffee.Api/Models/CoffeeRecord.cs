cd69653c1985915e2209b7327771ffdc0090b168
pablo-the-souza <53923086+pablo-the-souza@users.noreply.github.com>
2022-06-06 20:48:44 +1000

    Repository Get Method and Beginning Controller

---
+++ Coffee.Api/Coffee.Api/Models/CoffeeRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace Coffee.Api.Models
+{
+    public class CoffeeRecord
+    {
+        public int? Id { get; set; }
+        public string Type { get; set; }
+        public string? Bean { get; set; }
+        public string Location { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int Score { get; set; }
+        public int NoOfShots { get; set; }
+        public Single Price { get; set; }
+    }
+}
+
---
+++ Coffee.Api/Coffee.Api/Repositories/CoffeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using Coffee.Api.Models;
+using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace Coffee.Api.Repositories
+{
+    public interface ICoffeeRepository
+    {
+        List<CoffeeRecord> Get();
+    }
+
+    public class CoffeeRepository: ICoffeeRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public CoffeeRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public List<CoffeeRecord> Get()
+        {
+            using (IDbConnection connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                var query =
+                    @"SELECT * FROM Records";
+
+                var allTransactions = connection.Query<CoffeeRecord>(query);
+
+                return allTransactions.ToList();
+            }
+        }
+    }
+}
+
